db9c4756eba02d86be73899f7a20df04f88089ea
branden9 <73717120+branden9@users.noreply.github.com>
2026-03-23 14:51:23 -0500

    Committing 1 item from workspace 26f84b3b-c936-4482-b883-db691ee83597

---
+++ test.Notebook/notebook-content.py
@@ -0,0 +1,23 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+VariableLib = notebookutils.variableLibrary.getLibrary("var-library")
+
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
